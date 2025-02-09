903279cff739b1d54ea7c25b057df2bce379faa2
Lee Ye Seo <rosefor2ne@gmail.com>
2025-02-09 12:36:16 +0900

    design: font/color 시스템 셋팅

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="stylesheet preload" as="style" href="/font/font.css" />
+        <link rel="stylesheet" href="/css/globals.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,41 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        slate: {
+          900: "#0F172A",
+          800: "#1E293B",
+          500: "#64748B",
+          400: "#94A3B8",
+          300: "#CBD5E1",
+          200: "#E2E8F0",
+          100: "#F1F5F9",
+        },
+        violet: {
+          600: "#7C3AED",
+          100: "#EDE9FE",
+        },
+        rose: {
+          500: "#F43F5E",
+        },
+        lime: {
+          300: "#BEF264",
+        },
+        amber: {
+          800: "#92400E",
+        },
+      },
+      fontFamily: {
+        snas: ["NanumSquare", "Arial", "sans-serif"],
+      },
+      font: {
+        16: "16px",
+        18: "18px",
+        20: "20px",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+        extra: "800",
       },
     },
   },
